4964c1767ff787b89e3fd9fa79b43d2854423e8d
DREXYOP <91460817+DREXYOP@users.noreply.github.com>
2023-08-21 15:28:31 +0000

    Pending changes exported from your codespace

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-console.log(new Date().toLocaleString().split(", "))
+const News = require("./src/database/schemas/news")
+const mongoose = require("mongoose");
+require("./src/database/connect.cjs")
+const news = new News({
+    _id: new mongoose.Types.ObjectId,
+    title: "req.body.title",
+    description: "req.body.description",
+    author: "req.body.author",
+    location: "req.body.location",
+    // timeofUpload: new Date().toLocaleString(),
+    imageUrl: "mhgfds"
+  })
+news.save();
