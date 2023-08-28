37ae41ed2051d4555601c600dbcc26d5cc94bc24
DREXYOP <debadithyaraybarman257@gmail.com>
2023-08-28 14:01:11 +0000

    increased ratelimit to 10 requests

--- src/ratelimitMiddleWare.js
+++ src/ratelimitMiddleWare.js
@@ -2,7 +2,7 @@
 const RateLimit = require("express-rate-limit");
 const limiter = RateLimit({
   windowMs: 1*60*1000, // 1 minute
-  max: 3,
+  max: 10,
   delayMs: 0, // disable delaying - full speed until the max limit is reached
   message: "Too many requests maid from this IP, please try again after an hour",
   skipFailedRequests : true,
